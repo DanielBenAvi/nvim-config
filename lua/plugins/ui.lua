d6bacc2cf01761b2464ff9846d9b4cd5c671a875
Daniel Ben-Avi <dyfam@Daniels-MacBook-Air.local>
2025-07-17 22:07:33 +0300

    refactor plugins

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("bufferline").setup({})
+    end,
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup({
+        renderer = {
+          icons = {
+            show = {
+              file = true,
+              folder = true,
+              folder_arrow = true,
+              git = true,
+            },
+          },
+        },
+      })
+    end,
+  },
+}
