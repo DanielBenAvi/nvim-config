fc997dd3e9173c81925a252b0e0714c92481045a
Super User <root@localhost.localdomain>
2025-07-17 14:29:42 +0300

    init

---
+++ lua/plugins/init.lua
@@ -0,0 +1,65 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup({
+        renderer = {
+          icons = {
+            show = {
+              file = true,
+              folder = true,
+              folder_arrow = true,
+              git = true,
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+  -- LSP setup
+  "neovim/nvim-lspconfig",
+  config = function()
+    require("lspconfig").pylsp.setup({})
+  end,
+},
+
+{
+  -- Completion engine
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+  },
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+      }),
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+      },
+    })
+
+    -- Enhance LSP capabilities for completion
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    require("lspconfig").pylsp.setup({ capabilities = capabilities })
+  end,
+}
+
+}
+
