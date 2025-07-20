d24e8202d6c93b9ef9c6fe405c1858f81b2b8490
Super User <root@localhost.localdomain>
2025-07-20 08:25:41 +0300

    update

---
+++ lua/plugins/random.lua
@@ -0,0 +1,22 @@
+-- lua/plugins/whichkey.lua or part of keymaps.lua
+return
+{
+  {
+    "echasnovski/mini.icons",
+    version = false,
+    config = function()
+      require("mini.icons").setup()
+    end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = true,
+    event = "InsertEnter",
+  },
+  {
+    "numToStr/Comment.nvim",
+    config = true,
+    event = "VeryLazy",
+  }
+}
+
